603f88c058edd25853e2479a0e5e980a4dc36a26
Mehmet Vural <105605335+EsM3R@users.noreply.github.com>
2024-03-23 23:01:16 +0300

    Initial Commit

---
+++ E-CommerceApp/E_CommerceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  E_CommerceAppApp.swift
+//  E-CommerceApp
+//
+//  Created by Mehmet Vural on 23.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct E_CommerceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
